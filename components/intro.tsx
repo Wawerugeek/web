f41b3c627ca506a6f13b4ba1d38988faf6222bd5
Wawerugeek <stevemuiruri84@gmail.com>
2023-08-26 19:51:26 +0300

    changes

--- components/intro.tsx
+++ components/intro.tsx
@@ -133,7 +133,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/SteveCv.pdf"
+          href="/cv.pdf"
           download
         >
           Download CV{" "}
